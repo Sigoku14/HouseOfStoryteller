26419cbea9c01c810433fd39f7069124b3854ce4
至極 <sigoku14.murasaki@gmail.com>
2020-04-14 15:22:00 +0900

    カラー情報取得

--- resources/views/maps/map.blade.php
+++ resources/views/maps/map.blade.php
@@ -29,13 +29,13 @@
                     <div id="color">
                         @foreach ($colors as $color)
                                 <label class="color-box color-{{$i."-".$j}}" style="background-color: #{{$color->code}};">
-                                    <input type="radio" autocomplete="off" name="color-b" value="{{$color->name}}">
+                                    <input type="radio" autocomplete="off" name="color_b" value="{{$color->name}}">
                                 </label>
                         @endforeach
                         @if ($color->id == 1 )
                             @for ($i = 1; $i <= $cnt; $i++)
                                 <label class="color-box color-{{$i."-".$j}}" style="background-color: #ffffff;">
-                                    <input type="radio" autocomplete="off" name="color-b" value="white">
+                                    <input type="radio" autocomplete="off" name="color_b" value="white">
                                 </label>
                             @endfor
                         @endif
@@ -48,5 +48,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/@simonwep/selection-js/dist/selection.min.js"></script>
 <script type="text/javascript">
+        $(".color-box").change(function() {
+        var clr = $('[name=color_b]:checked').val();
+        console.log(clr);
+    })
 </script>
 @endsection
